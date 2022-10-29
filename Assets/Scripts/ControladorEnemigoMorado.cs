715baf984296381daba12899f7254d1b07d2b397
Biel Serrano Sanchez <bielsesa@gmail.com>
2022-10-29 21:28:25 +0200

    EnemigoMorado follows player smoothly

--- Assets/Scripts/ControladorEnemigoMorado.cs
+++ Assets/Scripts/ControladorEnemigoMorado.cs
@@ -1,17 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ControladorEnemigoMorado : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform objetivo;
+    public float rango = 1f;
+    public float velocidad = .5f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent("Rigidbody") as Rigidbody;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        float distancia = Vector3.Distance(objetivo.position, transform.position);
+
+        if (distancia <= rango)
+        {
+            // dispara al jugador
+            Disparar();
+        }
+        else
+        {
+            // se mueve hacia el jugador
+            Mover();
+        }
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Mover()
+    {
+
+        //Vector3 nuevaPosicion = Vector3.MoveTowards(transform.position, objetivo.transform.position, velocidad);
+        Vector3 nuevaPosicion = Vector3.Lerp(transform.position, objetivo.transform.position, velocidad * Time.fixedDeltaTime);
+        _rigidbody.MovePosition(nuevaPosicion);
+        transform.LookAt(objetivo);
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Disparar()
     {
         
     }
