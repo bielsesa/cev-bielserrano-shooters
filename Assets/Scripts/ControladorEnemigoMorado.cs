bbbd5a8b9e5c691fc01a61b51d52e770ffc60d63
Biel Serrano Sanchez <bielsesa@gmail.com>
2022-10-29 21:40:59 +0200

    EnemigoMorado creates raycast

--- Assets/Scripts/ControladorEnemigoMorado.cs
+++ Assets/Scripts/ControladorEnemigoMorado.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class ControladorEnemigoMorado : MonoBehaviour
 {
-
     public Transform objetivo;
+    public Transform pistola;
     public float rango = 1f;
     public float velocidad = .5f;
 
@@ -20,6 +20,7 @@ public class ControladorEnemigoMorado : MonoBehaviour
 
         if (distancia <= rango)
         {
+            Debug.Log("dentro de rango");
             // dispara al jugador
             Disparar();
         }
@@ -49,6 +50,15 @@ public class ControladorEnemigoMorado : MonoBehaviour
     /// </summary>
     void Disparar()
     {
-        
+        Debug.Log("dispara");
+        Ray rayOrigin = new Ray(pistola.position, Vector3.forward);
+        RaycastHit raycastHitInfo;
+
+        if (Physics.Raycast(rayOrigin, out raycastHitInfo))
+        {
+            Debug.Log(raycastHitInfo.transform.name);
+            Debug.Log(raycastHitInfo.transform.position);
+        }
+        Debug.Log("despues de raycast");
     }
 }
