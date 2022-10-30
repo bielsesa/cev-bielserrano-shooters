598eedbec91d34a821f865402f492c4e1c8eeaac
Biel Serrano Sanchez <bielsesa@gmail.com>
2022-10-30 17:52:39 +0100

    patrol EnemigoRojo with navmesh

--- Assets/Scripts/ControladorEnemigoRojo.cs
+++ Assets/Scripts/ControladorEnemigoRojo.cs
@@ -1,14 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ControladorEnemigoRojo : MonoBehaviour
 {
+    public NavMeshAgent agente;
+    [Header("Posiciones Patrulla")]
     public Transform puntoInicio;
     public Transform puntoFinal;
 
     void FixedUpdate()
     {
-        
+        if (IsInPosition(puntoFinal.position)) // && !agente.isStopped
+        {
+#if DEBUG
+            Debug.Log("punto final");
+#endif
+            agente.SetDestination(puntoInicio.position);
+        }
+        else if (IsInPosition(puntoInicio.position)) // && !agente.isStopped
+        {
+#if DEBUG
+            Debug.Log("punto inicial");
+#endif
+            agente.SetDestination(puntoFinal.position);
+        }
+    }
+
+    bool IsInPosition(Vector3 point)
+    {
+        if (transform.position.x == point.x
+            && transform.position.z == point.z)
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
     }
 }
